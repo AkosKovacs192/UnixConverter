e20061396c3a02a96685261f369e4b0d3fab6245
AkosKovacs192 <102877635+AkosKovacs192@users.noreply.github.com>
2023-07-03 21:20:20 +0100

    Initial Commit

---
+++ UnixConverter/UnixConverter.cs
@@ -0,0 +1,18 @@
+﻿
+
+static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+{
+     
+    DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+    dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+    return dateTime;
+}
+static double ToDateTimeFromUnixTimeStamp(DateTime datetime)
+{
+    return((DateTimeOffset)datetime).ToUnixTimeSeconds();
+}
+static double ToDateOnlyFromUnixTimeStamp(DateOnly dateOnly)
+{    
+    return ((DateTimeOffset) new DateTime(dateOnly.Year, dateOnly.Month, dateOnly.Day) ).ToUnixTimeSeconds();
+}
+
